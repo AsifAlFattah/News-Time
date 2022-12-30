6451a41978bd35c410f6ef4f966c760d2535065f
Asif Al Fattah <asif.alfattah.2@gmail.com>
2022-12-30 23:35:05 +0600

    Show News

--- scripts/app.js
+++ scripts/app.js
@@ -48,15 +48,40 @@ const showNews = (newses) =>{
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('col-12','d-flex', 'p-2', 'm-3', 'bg-white', 'rounded');
         newsDiv.innerHTML = `
-            <img src="${news.image_url}" alt=" " width="400px" height="400px" class="img p-3 rounded">
-            <div class="p-4 ">
-                <h4>${news.title}</h4>
+            <img src="${news.thumbnail_url}" alt=" " width="300px" height="300px" class="img p-3 rounded">
+            <div class="p-4 d-flex flex-column align-content-between">
+                <h4 class="fw-semibold mb-5">${news.title}</h4>
+                <p>${news.details.slice(0,400)}...</p>
+                <div class="d-flex justify-content-between">
+                    <div class="author-div d-flex">
+                        <img src="${news.author.img}" alt=" " width="50px" height="50px" class="rounded-5 ">
+                        <div class="px-2">
+                            <h5 class="mb-0 pb-0">${news.author.name}</h5>
+                            <p>${news.author.published_date}</p>
+                        </div>
+                    </div>
+                    <div class="views-div">
+                        <p class="fw-semibold"><i class="fa-regular fa-eye mx-1"></i> ${news.total_view}</p>
+                    </div>
+                    <div class="rating div">
+                        <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <div class="details-div">
+                        <button class="border-0 bg-white" onclick="showDetails('${news._id}')">
+                            <i class="fa-solid fa-arrow-right fs-2 px-4 "></i>
+                        </button>
+                    </div>
+                </div>
             </div>
         `;
         newsContainer.appendChild(newsDiv);
     }
 };
 
-
+// Show Detailed Post
+const showDetails = (id) =>{
+    const url = `https://openapi.programming-hero.com/api/news/${id}`
+    console.log(url);
+};
 
 loadAllCategories();
